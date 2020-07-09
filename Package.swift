b479c70f1011e4ea945956b2bc07ab1f82c075e6
Eneko Alonso <eneko.alonso@gmail.com>
2020-07-09 08:01:08 -0700

    Rename Bars to Stripes

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Bars",
+    name: "Stripes",
     platforms: [
         .macOS(.v10_15),
         .iOS(.v13),
@@ -14,8 +14,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Bars",
-            targets: ["Bars"]),
+            name: "Stripes",
+            targets: ["Stripes"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -25,7 +25,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Bars",
+            name: "Stripes",
             dependencies: [])
     ]
 )
